afe49069f7479779f989e0a9004771d968adbf47
emanuele <emanuele@relativita.com>
2012-08-09 12:34:23 +0200

    Refactoring

--- posterior_probability.py
+++ posterior_probability.py
@@ -3,6 +3,22 @@ from partial_independence import compute_logp_H
 from icann2011_confusion_matrices import tu
 from partitioner import Partition
 
+def compute_log_posteriors(X, partitions, alpha, prior_H):
+    """Compute log of p(H|X) for all the Hs, i.e. the partitions, of
+    the classes, given the confusion matrix X, the Dirichlet prior
+    alpha and the hypotheses' prior p(H) (prior_H).
+    """
+    logp_X_given_H = np.zeros(len(partitions))    
+    for i, partition in enumerate(partitions):
+        logp_X_given_H[i] = compute_logp_H(X, partition, alpha=alpha)
+
+    # normalization constant: p(X)
+    logp_X = reduce(np.logaddexp, logp_X_given_H + np.log(prior_H))
+    # p(H|X) from Bayes rule:
+    log_posterior_H_given_X = logp_X_given_H + np.log(prior_H) - logp_X
+    return log_posterior_H_given_X
+
+
 if __name__ == '__main__':
 
     X = tu
@@ -15,17 +31,9 @@ if __name__ == '__main__':
 
     # uniform prior on hypotheses: p(H_i)
     prior_H = np.ones(len(partitions)) / len(partitions)
-    logp_X_given_H = np.zeros(len(partitions))
+
+    log_posterior_H_given_X = compute_log_posteriors(X, partitions, alpha, prior_H)
     
-    for i, partition in enumerate(partitions):
-        logp_X_given_H[i] = compute_logp_H(X, partition, alpha=alpha)
-
-    # normalization constant: p(X)
-    logp_X = reduce(np.logaddexp, logp_X_given_H + np.log(prior_H))
-
-    # p(H|X) from Bayes rule:
-    log_posterior_H_given_X = logp_X_given_H + np.log(prior_H) - logp_X
-
     idx = np.argsort(log_posterior_H_given_X)[::-1]
 
     print
