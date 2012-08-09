9caae0cc0dabee2efa729bf48982d1bc573a7d47
emanuele <emanuele@relativita.com>
2012-08-09 12:41:48 +0200

    Added computation of partitions within posteriors

--- posterior_probability.py
+++ posterior_probability.py
@@ -3,11 +3,15 @@ from partial_independence import compute_logp_H
 from icann2011_confusion_matrices import tu
 from partitioner import Partition
 
-def compute_log_posteriors(X, partitions, alpha, prior_H):
+def compute_log_posteriors(X, partitions, alpha, prior_H=None):
     """Compute log of p(H|X) for all the Hs, i.e. the partitions, of
     the classes, given the confusion matrix X, the Dirichlet prior
     alpha and the hypotheses' prior p(H) (prior_H).
     """
+    if prior_H is None:
+        print "Assuming uniform prior for p(H_i)."
+        partitions = list(Partition(range(X.shape[0])))
+        prior_H = np.ones(len(partitions)) / len(partitions)
     logp_X_given_H = np.zeros(len(partitions))    
     for i, partition in enumerate(partitions):
         logp_X_given_H[i] = compute_logp_H(X, partition, alpha=alpha)
@@ -16,7 +20,7 @@ def compute_log_posteriors(X, partitions, alpha, prior_H):
     logp_X = reduce(np.logaddexp, logp_X_given_H + np.log(prior_H))
     # p(H|X) from Bayes rule:
     log_posterior_H_given_X = logp_X_given_H + np.log(prior_H) - logp_X
-    return log_posterior_H_given_X
+    return log_posterior_H_given_X, partitions
 
 
 if __name__ == '__main__':
@@ -32,7 +36,7 @@ if __name__ == '__main__':
     # uniform prior on hypotheses: p(H_i)
     prior_H = np.ones(len(partitions)) / len(partitions)
 
-    log_posterior_H_given_X = compute_log_posteriors(X, partitions, alpha, prior_H)
+    log_posterior_H_given_X, partitions = compute_log_posteriors(X, partitions, alpha, prior_H)
     
     idx = np.argsort(log_posterior_H_given_X)[::-1]
 
